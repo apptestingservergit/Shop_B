0a7bd3640e2ed1c99e5825222166677f29d7edb5
apptestingservergit <lek08670@gmail.com>
2026-08-31 12:19:20 +0700

    update new code

--- utils/emailHelper.js
+++ utils/emailHelper.js
@@ -3,23 +3,22 @@ const nodemailer = require('nodemailer');
 const sendEmail = async (options) => {
     const transporter = nodemailer.createTransport({
         host: 'smtp-relay.brevo.com',
-        port: 587,
-        secure: false, // Bắt buộc false đối với port 587
+        port: 465,
+        secure: true, // Bắt buộc true đối với cổng 465
         auth: {
-            user: process.env.EMAIL_USER, // Vẫn giữ nguyên tài khoản Login Brevo (b73caf001@smtp-brevo.com)
-            pass: process.env.EMAIL_PASS  // Vẫn giữ nguyên SMTP Key từ Render
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS // Lúc này dùng lại chuỗi xsmtpsib-... bình thường
         }
     });
 
     const mailOptions = {
-        // BẮT BUỘC ĐỂ EMAIL BẠN VỪA VERIFY TRÊN BREVO Ở ĐÂY ĐỂ TRÁNH BỊ CHẶN
         from: `"YOUTH SHOP" <lek08670@gmail.com>`,
         to: options.email,
         subject: options.subject,
         html: options.html
     };
 
-    await transporter.sendMail(mailOptions);
+    return await transporter.sendMail(mailOptions);
 };
 
 module.exports = sendEmail;
